28f40a7b847b56d2eacc43251b0af9ce761e5852
DelfiLinares <166736456+DelfiLinares@users.noreply.github.com>
2025-05-04 14:56:47 -0300

    Add files via upload

---
+++ customer.java
@@ -0,0 +1,19 @@
+public class customer {
+    String name;
+
+    public customer(String name) {
+        this.name = name;
+    }
+
+    public customer(){
+        this.name = "Pepo";
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
